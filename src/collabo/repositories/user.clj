08aa6fd5023b3e4a60f38e264d3c956d915e5950
Arakaki-Yuji <arakaki@ryukyu-i.co.jp>
2018-02-04 08:00:14 +0900

    save password that is generated hash from plain password.

--- src/collabo/repositories/user.clj
+++ src/collabo/repositories/user.clj
@@ -2,11 +2,12 @@
   (:require [clojure.java.jdbc :as j]
             [collabo.config.db :refer [db]]
             [clj-time.local :as tl]
-            [clj-time.jdbc]))
+            [clj-time.jdbc]
+            [buddy.hashers :as hashers]))
 
 (defn create-user [email account_name password]
   (j/insert! db :users {:email email
                         :account_name account_name
-                        :password password
+                        :password (hashers/derive password)
                         :created_at (tl/local-now)
                         :updated_at (tl/local-now)}))
