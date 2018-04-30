9d7dfa8427388330312d6785a7b0bcf60d12b6c9
Arakaki-Yuji <arakaji0830@gmail.com>
2018-05-01 00:55:00 +0900

    add setting design.

--- src/collabo/handlers/user.clj
+++ src/collabo/handlers/user.clj
@@ -2,9 +2,16 @@
   (:require [collabo.views.user :as v-user]
             [collabo.handlers.base :refer [html]]
             [collabo.models.user :as m-user]
-            [collabo.repositories.project :as pj-repo]))
+            [collabo.repositories.project :as pj-repo]
+            [clojure.tools.logging :as log]))
 
-(defn get-user [{:keys [route-params]}]
+(defn get-user [{:keys [route-params query-params] :as req}]
   (let [user (m-user/find-by-identity (:account_name route-params))
         user-projects (pj-repo/find-owned-projects (:id user))]
-    (html (v-user/user-page user user-projects))))
+    (do
+     (log/info (get query-params "tab"))
+     (log/info (type (first (keys query-params))))
+     (html (v-user/user-page user
+                             user-projects
+                             (get query-params "tab")
+                             (get query-params "menu"))))))
--- src/collabo/views/user.clj
+++ src/collabo/views/user.clj
@@ -1,9 +1,91 @@
 (ns collabo.views.user
   (:require [hiccup.core :as h]
             [collabo.views.layout :refer [layout]]
-            [collabo.views.components.home.project-list :refer [project-list]]))
+            [collabo.views.components.home.project-list :refer [project-list]]
+            [clojure.string :refer [join]]))
 
-(defn user-page [user projects]
+(defn make-query-string [queries]
+  (if (< 0 (count queries))
+    (str "?" (join "&" queries))))
+
+(defn make-link
+  ([user]
+   (make-link user nil nil))
+  ([user tab]
+   (make-link user tab nil))
+  ([user tab menu]
+   (let [tab-query (if tab (str "tab=" (name tab)))
+         menu-query (if menu (str "menu=" (name menu)))]
+     (str "/users/"
+          (:account_name user)
+          (make-query-string (filter identity [tab-query menu-query]))
+          ))))
+
+(defn projects-component [projects]
+  [:div {:class "columns"}
+   [:div {:class "column col-8 col-mx-auto"}
+    (project-list projects)
+    ]])
+
+(defn edit-email-cmp []
+  [:form
+   [:div {:class "form-group"}
+    [:label {:class "form-label" :for "user-current-email"} "Current Email"]
+    [:input {:class "form-input" :type "email" :id "user-current-email" :name "user-current-email"}]
+    ]
+   [:div {:class "form-group"}
+    [:label {:class "form-label" :for "user-new-email"} "New Email"]
+    [:input {:class "form-input" :type "email" :id "user-new-email" :name "user-new-email"}]
+    ]
+   ])
+
+(defn edit-aboutme-cmp []
+  [:form
+   [:div {:class "form-group"}
+    [:label {:class "form-label" :for "user-aboutme"} "About me"]
+    [:textarea {:class "form-input" :id "user-aboutme" :name "user-aboutme" :rows 5}]
+    ]
+   ])
+
+(defn edit-icon-cmp []
+  [:form
+   [:div {:class "form-group"}
+    [:label {:class "form-label" :for "user-icon"} "Current Icon"]
+    [:input {:class "form-input" :type "file" :id "user-icon" :name "user-icon"}]
+    ]
+   ])
+
+(defn setting-component [title menu body-comp user]
+  [:div {:class "columns"}
+   [:div {:class "column col-4"}
+    [:ul {:class "menu"}
+     [:li {:class "divider" :data-content "SETTING MENU"}]
+     [:li {:class "menu-item"}
+      [:a {:href (make-link user :setting :email) :class (if (= menu :email) "active")} "Edit Email"]
+      ]
+     [:li {:class "menu-item"}
+      [:a {:href (make-link user :setting :aboutme) :class (if (= menu :aboutme) "active")} "Edit About me"]
+      ]
+     [:li {:class "menu-item"}
+      [:a {:href (make-link user :setting :icon)
+           :class (if (= menu :icon) "active")} "Edit Profile icon"]
+      ]
+     ]
+    ]
+   [:div {:class "column col-8"}
+    [:div {:class "panel"}
+     [:div {:class "panel-header"}
+      [:h2 {:class "text-center"} title]]
+     [:div {:class "panel-body"}
+      body-comp
+      ]
+     [:div {:class "panel-footer"}
+      [:button {:class "btn btn-primary btn-block"} "UPDATE"]]
+     ]
+    ]]
+  )
+
+(defn user-page [user projects tab menu]
   (layout
    [:div {:class "user-page"}
     [:div {:class "columns"}
@@ -13,7 +95,7 @@
         [:figure {:class "avatar avatar-xl" :data-initial "YJ"}
          [:img {:src "/images/profile-icon.jpg"}]
          ]]
-       [:div {:class "column col-10 "}
+       [:div {:class "column col-10"}
         [:h2 {:class "text-bold"} (:account_name user)]
         [:p "Software Enginner in Okinawa. Founder of Collabo. member of Javakueche. Husband, Father."]
         ]
@@ -23,25 +105,23 @@
     [:div {:class "divider"}]
     [:div {:class "columns"}
      [:ul {:class "tab tab-block colum col-12 col-mx-auto"}
-      [:li {:class "tab-item"}
-       [:a {:href "#"} "Timeline"]
+      [:li {:class (str "tab-item" (if (= (keyword tab) :projects) " active"))}
+       [:a {:href (make-link user :projects)} "Projects"]
        ]
-      [:li {:class "tab-item active"}
-       [:a {:href "#"} "Projects"]
-       ]
-      [:li {:class "tab-item"}
-       [:a {:href "#"} "Profile"]
-       ]
-      [:li {:class "tab-item"}
-       [:a {:href "#"} "Setting"]
+      [:li {:class (str "tab-item" (if (= (keyword tab) :setting) " active"))}
+       [:a {:href (make-link user :setting)} "Setting"]
        ]
       ]
      ]
-    [:div {:class "columns"}
-     [:div {:class "column col-8 col-mx-auto"}
-      (project-list projects)
-      ]
-     ]
+    (case (keyword tab)
+      :projects (projects-component projects)
+      :setting (case (keyword menu)
+                 :aboutme (setting-component "Edit Aboutme" (keyword menu) (edit-aboutme-cmp) user)
+                 :email (setting-component "Edit Email" (keyword menu) (edit-email-cmp) user)
+                 :icon (setting-component "Edit Icon" (keyword menu) (edit-icon-cmp) user)
+                 (setting-component "Edit Email" (keyword menu) (edit-email-cmp) user))
+      (projects-component projects)
+      )
     ]
    )
   )
