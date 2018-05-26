11d763e8a527184580f46643d8af8e8d192ba031
Arakaki-Yuji <arakaji0830@gmail.com>
2018-05-27 08:22:18 +0900

    fix bug

--- src/collabo/handlers/utilities/project.clj
+++ src/collabo/handlers/utilities/project.clj
@@ -1,6 +1,8 @@
 (ns collabo.handlers.utilities.project)
 
 (defn current-user-is-owner [{:keys [identity] :as session} project]
-  (if (= identity (:account_name project))
-    true
-    false))
+  (if-not identity
+    false
+    (if (= (name identity) (:account_name project))
+      true
+      false)))
