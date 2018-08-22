8b28b5c609775967e8ac481715e71b9a72b4bb7c
Arakaki-Yuji <arakaji0830@gmail.com>
2018-08-22 13:01:22 +0900

    timezone change Asia/Tokyo

--- src/collabo/views/utilities/datetime.clj
+++ src/collabo/views/utilities/datetime.clj
@@ -3,6 +3,6 @@
             [clj-time.format :as tf]))
 
 (defn datetime-format [datetime]
-  (tf/unparse (tf/formatter-local "yyyy-MM-dd HH:mm:ss") (tc/to-time-zone datetime (tc/default-time-zone))))
-
+  (tf/unparse (tf/formatter-local "yyyy-MM-dd HH:mm:ss")
+              (tc/to-time-zone datetime (tc/time-zone-for-id "Asia/Tokyo"))))
 
